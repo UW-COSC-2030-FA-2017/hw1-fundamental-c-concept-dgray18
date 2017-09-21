07277886a84c1c207272bc7391f6c398e1819e9d
dgray18 <31670425+dgray18@users.noreply.github.com>
2017-09-21 16:36:08 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,27 +1,99 @@
-/* 
- * File:   main.cpp
- * Author: dustin
- *
- * Created on September 5, 2017, 6:14 PM
- */
-
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <string>
+
 using namespace std;
 
-class object {
-public:
-object();
-~object();
+
+class genericClass {
+   public:
+        int number;
+        string name;
 };
+vector<genericClass> globalVector;
 
-object::object(void){
-     std:cout << "constructor is run";
-}
-object::~object(void){
-    std:cout << " object is deleted";
+//functions
+void notContained(){
+cout << "this item is not contained" << endl;
 }
 
-int main(){
-    object object;
+//empties collection
+void makeEmpty(){
+    for(int i = 0; i < globalVector.size(); i++){
+    globalVector.erase(globalVector.begin()+i);
+    }
+}
+
+//inserts into vector
+void insertVector(int item, string name){
+    genericClass genericObject;
+    genericObject.number = item;
+    genericObject.name = name;
+    globalVector.push_back(genericObject);
+}
+
+
+void vectorRemove(int position){
+    if(position > globalVector.size()){
+    notContained();
+    }
+    else if(position < 0){
+    notContained();
+    }
+    else{
+    globalVector.erase(globalVector.begin() + position);
+    }
+}
+
+int removeRandom(){
+
+    srand((unsigned)time(0));
+    int randNum;
+
+    randNum = (rand()%globalVector.size())+0;
+
+    globalVector.erase(globalVector.begin() + randNum);
+}
+
+
+int main()
+{
+
+
+//    insertVector(genericObject);
+    insertVector(0, "obj0");
+    insertVector(1, "obj1");
+    insertVector(2, "obj2");
+    insertVector(3, "obj3");
+
+    for(int i = 0; i < globalVector.size(); i ++){
+    cout << globalVector[i].number << ": " << globalVector[i].name;
+    }
+    cout << endl;
+    removeRandom();
+
+        for(int i = 0; i < globalVector.size(); i ++){
+    cout << globalVector[i].number << ": " << globalVector[i].name;
+    }
+
+    cout << endl;
+
+    vectorRemove(19);
+    vectorRemove(2);
+
+        for(int i = 0; i < globalVector.size(); i ++){
+    cout << globalVector[i].number << ": " << globalVector[i].name;
+    }
+
+    cout << endl;
+
+    makeEmpty();
+
+        for(int i = 0; i < globalVector.size(); i ++){
+    cout << globalVector[i].number << ": " << globalVector[i].name;
+    }
+
+
     return 0;
-};
+}
